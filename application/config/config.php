ca4f400fa76c155408b1002b29c3adb98ce05b23
Yanis DANDO <yanis.dando@gmail.com>
2018-02-28 22:06:16 +0100

    Allow dynamic page changing

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://pure-inlet-22307.herokuapp.com/';
+//$config['base_url'] = 'https://pure-inlet-22307.herokuapp.com/';
+$config['base_url'] = 'http://localhost/BookOfFate/';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'Controller_Accueil';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['changePage/(:any)'] = 'changePage/$1';
